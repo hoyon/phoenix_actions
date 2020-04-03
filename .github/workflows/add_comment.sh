552dc253da3f7b07c0942395dc30d22c3efa62a3
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-03 16:53:58 +0100

    trying to add check

--- .github/workflows/add_comment.sh
+++ .github/workflows/add_comment.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-hub api /repos/hoyon/phoenix_actions/issues/1/comments --field body="hello from actions"
+# hub api /repos/hoyon/phoenix_actions/issues/1/comments --field body="hello from actions"
+hub api /repos/hoyon/phoenix_actions/check-runs -H Accept:"application/vnd.github.antiope-preview+json" --field name="check run" --field head_sha="$GITHUB_SHA"
