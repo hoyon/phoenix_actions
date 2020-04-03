4d102e06bf9463cec719e29450bff8772b5f70b5
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-03 17:11:15 +0100

    fixing sedError: target STRING not available

--- .github/workflows/add_comment.sh
+++ .github/workflows/add_comment.sh
@@ -2,6 +2,8 @@
 
 # hub api /repos/hoyon/phoenix_actions/issues/1/comments --field body="hello from actions"
 pwd
-sed "s/\__SHA__/$GITHUB_SHA" ./input.json
+cd .github/workflows || exit
+
+sed "s/\__SHA__/$GITHUB_SHA/" ./input.json
 
 hub api /repos/hoyon/phoenix_actions/check-runs -H Accept:"application/vnd.github.antiope-preview+json" --input=./input.json
