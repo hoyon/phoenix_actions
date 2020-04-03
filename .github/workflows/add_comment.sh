a7e262f0102cc10c0d631a5eaca7535b803f3b19
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-03 17:09:40 +0100

    using input.json

--- .github/workflows/add_comment.sh
+++ .github/workflows/add_comment.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # hub api /repos/hoyon/phoenix_actions/issues/1/comments --field body="hello from actions"
-hub api /repos/hoyon/phoenix_actions/check-runs -H Accept:"application/vnd.github.antiope-preview+json" --field name="check run" --field head_sha="$GITHUB_SHA" --field status=completed --field conclusion=failure --field output='{"title":"my test","summary":"something went wrong"}'
+pwd
+sed "s/\__SHA__/$GITHUB_SHA" ./input.json
+
+hub api /repos/hoyon/phoenix_actions/check-runs -H Accept:"application/vnd.github.antiope-preview+json" --input=./input.json
