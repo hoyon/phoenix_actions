7af1ededff18ba6beeb6936b300a63e9f3e0d07c
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-03 17:12:22 +0100

    adding -i

--- .github/workflows/add_comment.sh
+++ .github/workflows/add_comment.sh
@@ -4,6 +4,6 @@
 pwd
 cd .github/workflows || exit
 
-sed "s/\__SHA__/$GITHUB_SHA/" ./input.json
+sed -i "s/\__SHA__/$GITHUB_SHA/" ./input.json
 
 hub api /repos/hoyon/phoenix_actions/check-runs -H Accept:"application/vnd.github.antiope-preview+json" --input=./input.json
