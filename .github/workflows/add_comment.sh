7ac041fbed4532a12722ad2c1b7c6ee7a4d04586
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-03 17:00:16 +0100

    less escaping

--- .github/workflows/add_comment.sh
+++ .github/workflows/add_comment.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # hub api /repos/hoyon/phoenix_actions/issues/1/comments --field body="hello from actions"
-hub api /repos/hoyon/phoenix_actions/check-runs -H Accept:"application/vnd.github.antiope-preview+json" --field name="check run" --field head_sha="$GITHUB_SHA" --field status=completed --field conclusion=failure --field output="{\"title\":\"my test\",\"summary\":\"something went wrong\"}"
+hub api /repos/hoyon/phoenix_actions/check-runs -H Accept:"application/vnd.github.antiope-preview+json" --field name="check run" --field head_sha="$GITHUB_SHA" --field status=completed --field conclusion=failure --field output='{"title":"my test","summary":"something went wrong"}'
