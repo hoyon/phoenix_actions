26324b9b676094ea967f36de549ab2e85f5ce4d2
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-03 17:39:12 +0100

    using different sha

--- .github/workflows/add_comment.sh
+++ .github/workflows/add_comment.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # hub api /repos/hoyon/phoenix_actions/issues/1/comments --field body="hello from actions"
-git show
+git rev-parse HEAD
 cd .github/workflows || exit
 
 echo "$GITHUB_SHA"
+echo "$COMMIT_SHA"
 
-sed -i "s/\__SHA__/$GITHUB_SHA/" ./input.json
+sed -i "s/\__SHA__/$COMMIT_SHA/" ./input.json
 
 hub api /repos/hoyon/phoenix_actions/check-runs -H Accept:"application/vnd.github.antiope-preview+json" --input=./input.json
