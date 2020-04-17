444ff760ccbbb7b2dc363d095d59ec95133e69c0
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-17 17:00:51 +0100

    using correct env var

--- .github/workflows/add_comment.sh
+++ .github/workflows/add_comment.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 python ./.github/workflows/linter.py > input.json
 
-curl -H "Authorization: token ${API_TOKEN}" \
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
     -H "Accept: application/vnd.github.antiope-preview+json" \
     -XPOST \
     --data '@input.json' \
