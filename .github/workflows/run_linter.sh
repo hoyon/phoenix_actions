c75c81082cba521cd4aef6a9074a7ad1b3374c83
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-17 17:45:02 +0100

    removing comment

--- .github/workflows/run_linter.sh
+++ .github/workflows/run_linter.sh
@@ -4,10 +4,3 @@ set -e
 
 echo "::add-matcher::.github/workflows/linter-matcher.json"
 python ./.github/workflows/linter.py
-
-
-# curl -H "Authorization: token ${GITHUB_TOKEN}" \
-#     -H "Accept: application/vnd.github.antiope-preview+json" \
-#     -XPOST \
-#     --data '@input.json' \
-#     "https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs"
